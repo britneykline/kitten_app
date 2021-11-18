026a45e4c13eb0fd5ca4ed944b9d50fba0356840
britneykline <87348224+britneykline@users.noreply.github.com>
2021-11-17 20:22:42 -0500

    create index action, create create action

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,18 @@
+class KittensController < ApplicationController
+  def index
+    kittens = Kitten.all 
+    render json: kittens.as_json
+  end
+
+  def create
+    kitten = Kitten.new(
+      name: params[:name],
+      breed: params[:breed],
+      age: params[:age],
+      image: params[:image]
+    )
+    kitten.save
+    render json: kitten.as_json
+  end
+
+end
---
+++ kittens-vue-app/src/views/KittensIndex.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="kittens-index">
+    <h1>All Kittens</h1>
+    <div v-for="kitten in kittens" v-bind:key="kitten.id">
+      <h2>{{ kitten.name }}</h2>
+      <img v-bind:src="kitten.image" v-bind:alt="kitten.name" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      kittens: [],
+    };
+  },
+  created: function () {
+    this.indexKittens();
+  },
+  methods: {
+    indexKittens: function () {
+      axios.get("/kittens").then((response) => {
+        console.log("kittens index", response);
+        this.kittens = response.data;
+      });
+    },
+  },
+};
+</script>
