546ceee53f9b715738d715e3fb42dfa2e88bd366
britneykline <87348224+britneykline@users.noreply.github.com>
2021-11-28 12:53:03 -0500

    db seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Kitten.create!([
+  {name: "Ginger", breed: "Scottish Fold", age: 1, image: "https://tse4.mm.bing.net/th?id=OIP.JHxr9gM-PiOZgjCKRVTxtAHaEK&pid=Api&P=0&w=357&h=202"},
+  {name: "Harley", breed: "British Shorthair", age: 5, image: "https://tse2.mm.bing.net/th?id=OIP.AMStN1tgo2OAJGXElYFVpwHaEK&pid=Api&P=0&w=342&h=193"},
+  {name: "Cupcake", breed: "Munchkin", age: 6, image: "https://www.zooplus.co.uk/magazine/wp-content/uploads/2018/07/Munchkin-Katze-768x562.jpg"}
+])
